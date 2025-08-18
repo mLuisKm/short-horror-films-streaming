aaf7954ddfa48048f3feeca5b48115d3adbec2f2
Luis7km <mluis.km@gmail.com>
2025-08-18 16:07:27 -0500

    Successfully removed every 'use lient' from upper levels components as pages and layouts

---
+++ components/SignOutButton/SignOutButton.jsx
@@ -0,0 +1,19 @@
+"use client"
+import { useSession, signOut } from "next-auth/react"
+
+export default function SignOutButton() {
+    const { data: session } = useSession()
+
+    if (!session) {
+        return null
+    }
+
+    return (
+        <div>
+            <h1>Hola, estas logueado</h1>
+            <button onClick={() => signOut()} className="sign-out-button">
+                Sign Out
+            </button>
+        </div>
+    )
+}
