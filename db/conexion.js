5928c83022dc462afb508d75586940c1bfa3ee94
Luis7km <mluis.km@gmail.com>
2025-08-25 03:08:06 -0500

    Restored to the last stable version with TLS connection to oracle

--- db/conexion.js
+++ db/conexion.js
@@ -1,8 +1,6 @@
 import oracledb from 'oracledb'
-import { initWalletFromEnv } from "./initWalletFromEnv.js";
 
 export async function getConnection() {
-    initWalletFromEnv();
     return await oracledb.getConnection({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
